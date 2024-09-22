223f1b6d970be6206a0cd412082fe610be6d9fe6
cc <claude.cockfield@bison.howard.edu>
2024-09-22 12:16:27 -0400

    backup

--- heap/src/main/java/cc/heap/Heap.java
+++ heap/src/main/java/cc/heap/Heap.java
@@ -2,7 +2,17 @@ package cc.heap;
 
 public class Heap {
 
-  private int[] _hash = new int[] { 5, 13, 2, 25, 7, 17, 20, 8, 4};
+ // private final int[] _hash = new int[] { 5, 13, 2, 25, 7, 17, 20, 8, 4 };
+
+  private final int[] _hash = new int[] { 5, 13, 2, 25, 7, 17, 20, 8, 4, 13, 2, 25, 7, 17, 20, 8, 4 };
+
+  private final int _width = 6;
+
+  private final int _height = (int) (Math.log(_hash.length) / Math.log(2));
+
+  private final int _leaves = (int) Math.pow(2, _height);
+  
+  private final int _line = (_width + 2) * _leaves;
 
   public Heap() {
   }
@@ -10,22 +20,94 @@ public class Heap {
   public String toString() {
     StringBuilder buffer = new StringBuilder();
 
-    buffer.append(toString(0));
+    buffer.append("total height " + _height);
+
+    buffer.append("\n");
+
+    buffer.append("leaves " + _leaves);
+
+    buffer.append("\n");
     
+    buffer.append(toString(0));
+
     return buffer.toString();
   }
 
   public static void main(String[] args) {
     System.out.println("start.");
-    
+
+    Heap h = new Heap();
+
+    System.out.println(h.toString());
+
     System.out.println("stop.");
   }
-  
+
   private String toString(int i) {
+    
+    if (i >= _hash.length) {
+
+      return "";
+    }
+ 
+    boolean debug = false;
+    
     StringBuilder buffer = new StringBuilder();
+
+    if (debug) {
+      buffer.append("i " + i);
+
+      buffer.append("\n");
+    }
     
-    buffer.append(_hash[0]);
+    int level = (int) (Math.log(i+1) / Math.log(2));
+
+    if (debug) {
     
-    return buffer.toString();
+      buffer.append("level " + level);
+
+      buffer.append("\n");
+    }
+    
+    int sections = (int) Math.pow(2, level) + 1;
+
+    
+    if (debug) {
+    
+      buffer.append("sections " + sections);
+
+      buffer.append("\n");
+    }
+    
+    int unit_width = _line / sections;
+    
+    if (debug) {
+      
+      buffer.append("unit_width " + unit_width);
+
+      buffer.append("\n");
+    }
+    
+    int element_on_lint = (i + 1) - ((int) Math.pow(2,  level) - 1);
+
+    if (debug) {
+      buffer.append("element_on_lint " + element_on_lint);
+
+      buffer.append("\n");
+    }
+    
+    buffer.append(String.format("%" + unit_width + "s", pad(_hash[i])));
+
+    buffer.append(toString(i + 1));
+        
+    return element_on_lint == 1 ? "\n" + buffer.toString() : buffer.toString();
+  }
+
+  private String pad(int i) {
+
+    int padding = _width - (int) Math.log(i);
+
+    return String.format("%-" + Integer.toString(padding) + "s",
+        String.format("%" + Integer.toString((int) (padding + 1) - ((int) padding / 2)) + "s", Integer.toString(i)));
   }
 }
